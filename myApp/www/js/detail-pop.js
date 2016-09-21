691c81ea3d0f900f3b55959c68a5bcb1fe31fa2c
aihaiti <baghrash1985@gmail.com>
2016-09-21 14:17:51 +0200

    Updated

--- myApp/www/js/detail-pop.js
+++ myApp/www/js/detail-pop.js
@@ -3,9 +3,9 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('detail-pop', ['ionic']) 
+angular.module('detail-pop', ['ionic'])
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform) { 
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
